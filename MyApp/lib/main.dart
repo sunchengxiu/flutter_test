709bd13d88e280463fd153297df4e0f79dd3826f
sunchegnxiu <sunchegnxiu>
2020-05-16 21:19:10 +0800

    分层

--- MyApp/lib/main.dart
+++ MyApp/lib/main.dart
@@ -2,27 +2,48 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text(
-              "app bar",
-              style: TextStyle(fontSize: 30),
-            ),
-          ),
-          body: Center(
-            child:Text(
-              "hello world",
-              textDirection: TextDirection.ltr,
-              style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.green
-              ),
-            ) ,
-          ),
-        )
-    )
-  );
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return  MaterialApp(
+        home: MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "app bar",
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+      body: MyHomeBody(),
+    );
+  }
+}
+
+class MyHomeBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return  Center(
+      child:Text(
+        "hello world",
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+            fontSize: 30,
+            color: Colors.green
+        ),
+      ) ,
+    );
+  }
 }
