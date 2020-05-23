5655dab0c3f2a48aeeeee61e71d928e9e94410ff
sunchegnxiu <sunchegnxiu>
2020-05-23 10:56:38 +0800

    list view

--- MyApp/lib/listView_test/listview_test.dart
+++ MyApp/lib/listView_test/listview_test.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -22,15 +21,48 @@ class MyApp extends StatelessWidget {
 class MyHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: List.generate(100, (index){
-        return ListTile(
-          leading: Icon(Icons.favorite),
-          trailing: Icon(Icons.delete),
-          title: Text("当前第$index行"),
-          subtitle: Text("subtitle : $index"),
-        );
-      })
+    return ListView.separated(
+        // 设置cell
+        itemBuilder: (BuildContext context , int index){
+          return Text("ListView.sepatated : $index");
+        },
+        // 设置线
+        separatorBuilder: (BuildContext context , int index){
+          return Divider(
+              color: Colors.black,
+              height: 50,
+              indent: 20,
+              endIndent: 20,
+            thickness: 1,
+          );
+        },
+        itemCount: 100
     );
+  }
+
+  ListView buildListViewBuilder() {
+    return ListView.builder(
+      itemCount: 100,
+      itemExtent: 100,
+      // 相当于懒加载，展示的时候就会调用这个builder
+      itemBuilder: (BuildContext context, int index) {
+        // 返回一个 widget
+        return Text("hello world : $index");
+      });
+  }
+
+  ListView buildListView() {
+    return ListView(
+        // 行高大小
+        itemExtent: 100,
+        // 一次性直接加载，一般简单布局，可以使用，相对性能较差一点
+        children: List.generate(100, (index) {
+          return ListTile(
+            leading: Icon(Icons.favorite),
+            trailing: Icon(Icons.delete),
+            title: Text("当前第$index行"),
+            subtitle: Text("subtitle : $index"),
+          );
+        }));
   }
 }
